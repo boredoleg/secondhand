f24e1fe9a144fa3ee509f84113e28cabbf697b85
Bored Oleg <boredoleg@gmail.com>
2013-10-03 11:02:55 +0800

    Solving the NAME migration problem

--- db/migrate/20130926020112_add_information_to_pins.rb
+++
@@ -1,8 +0,0 @@
-class AddInformationToPins < ActiveRecord::Migration
-  def change
-    add_column :pins, :name, :string
-    add_column :pins, :email, :string
-    add_column :pins, :phone, :string
-    add_column :pins, :price, :decimal
-  end
-end
